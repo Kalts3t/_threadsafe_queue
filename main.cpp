b8f86601b3a9fed16c1df3aa7d3d38a5a62112f2
Kalts3t <101403282+Kalts3t@users.noreply.github.com>
2024-10-24 19:48:00 +0800

    Update main.cpp
    可以并发的一个有限队列，使用了条件变量完成“有限”

--- main.cpp
+++ main.cpp
@@ -1,48 +1,114 @@
+
 #include <condition_variable>
-#include <iostream>
-#include <mutex>
-#include <queue>
+#include <memory>
+#include <thread>
 
 template<typename T>
-class threadsafe_queue {
-    private:
-    mutable std::mutex m;
+class queue_threadsafe {
+private:
+    struct node {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+    std::unique_ptr<node> head;
+    std::mutex head_mutex;
+    node* tail;
+    std::mutex tail_mutex;
     std::condition_variable cv;
-    std::queue<T> queue_;
-    public:
-    threadsafe_queue()=default;
-    threadsafe_queue& operator=(const threadsafe_queue& rhs)=delete;
-    threadsafe_queue(const threadsafe_queue& rhs)=delete;
-
-    threadsafe_queue(threadsafe_queue& other) {
-        std::unique_lock<std::mutex> lock(m);
-        queue_=other.queue_;
+    std::condition_variable queue_not_full;
+    std::atomic<size_t> current_size;
+    size_t max_size;
+    node* get_tail() {
+        std::unique_lock<std::mutex> lock(tail_mutex);
+        return tail;
+    }
+    std::unique_ptr<T> pop_head() {
+        std::unique_ptr<node> old_head(std::move(head));
+        head = std::move(old_head->next);
+        return old_head;
+    }
+    std::unique_lock<std::mutex> wait_for_data() {
+        std::unique_lock lock(head_mutex);
+        cv.wait(lock,[&]{return head.get()!=get_tail();});
+        return lock;
+    }
+    std::unique_ptr<T> wait_pop_head() {
+        std::unique_lock<std::mutex> lock(wait_for_data());
+        return pop_head();
+    }
+    std::unique_ptr<T> wait_pop_head(T& value) {
+        std::unique_lock<std::mutex> lock(wait_for_data());
+        value = std::move(*head->data);
+        return pop_head();
     }
 
-    void push(T item) {
-        std::unique_lock<std::mutex> lock(m);
-        queue_.push(item);
+    std::unique_ptr<T> try_pop_head() {
+        std::unique_lock<std::mutex> lock(head_mutex);
+        if (head.get() == get_tail()) {
+            return std::unique_ptr<T>();
+        }
+        return pop_head();
+    }
+    std::unique_ptr<T> try_pop_head(T& value) {
+        std::unique_lock<std::mutex> lock(head_mutex);
+        if (head.get() == get_tail()) {
+            return std::unique_ptr<T>();
+        }
+        value = std::move(*head->data);
+        return pop_head();
+    }
+
+    void check_capcity(std::unique_ptr<T> old_head) {
+        if(old_head) {
+            std::unique_lock<std::mutex> lock(tail_mutex);
+            --current_size;
+            queue_not_full.notify_one();
+        }
+    }
+public:
+    explicit queue_threadsafe(size_t max):max_size(max),current_size(0),head(new node),tail(head.get()){}
+    queue_threadsafe(const queue_threadsafe& other)=delete;
+    queue_threadsafe& operator=(const queue_threadsafe& other)=delete;
+    std::unique_ptr<T> wait_for_pop() {
+        std::unique_ptr<T> old_head = std::move(wait_pop_head());
+        check_capcity(old_head);
+        return old_head;
+    }
+    std::unique_ptr<T> wait_for_pop(T& value) {
+        std::unique_ptr<T> old_head = std::move(wait_pop_head(value));
+        value = std::move(*old_head->data);
+        check_capcity(old_head);
+        return old_head;
+    }
+    void push(T new_value) {
+        {
+            std::unique_ptr<T> new_data = std::make_unique<T>(std::move(new_value));
+            std::unique_ptr<T> new_node(new node());
+            std::unique_lock<std::mutex> lock(tail_mutex);
+            queue_not_full.wait(lock,[&]{return max_size>current_size;});
+            tail->data = std::move(new_data);
+            tail->next = std::move(new_node);
+            node* new_tail = new_node.get();
+            tail = new_tail;
+            ++current_size;
+        }
         cv.notify_one();
     }
 
-    T wait_and_pop() {
-        std::unique_lock<std::mutex> lock(m);
-        wait(lock, [&]{ return !queue_.empty(); });
-        auto item = queue_.front();
-        queue_.pop();
-        return item;
+    std::unique_ptr<T> try_pop() {
+        std::unique_ptr<T> old_head(try_pop_head());
+        check_capcity(old_head);
+        return old_head?old_head->data:std::unique_ptr<T>();
     }
 
-    std::shared_ptr<T> shared_wait_pop() {
-        std::unique_lock<std::mutex> lock(m);
-        wait(lock, [&]{ return !queue_.empty(); });
-        std::shared_ptr<T> item(make_shared<T>(queue_.front()));
-        queue_.pop();
-        return item;
+    bool try_pop(T& value) {
+        std::unique_ptr<T> old_head = std::move(try_pop_head(value));
+        check_capcity(old_head);
+        return old_head!=nullptr;
     }
 
-    bool empty() const {
-        std::unique_lock<std::mutex> lock(m);
-        return queue_.empty();
+    bool empty() {
+        std::unique_lock<std::mutex> lock(head_mutex);
+        return head.get()==get_tail();
     }
 };
